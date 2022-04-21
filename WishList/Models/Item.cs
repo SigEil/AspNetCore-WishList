1d58c5e856a96d864ea790998657af342d5c2685
Sigve Eilertsen <sigve42@live.no>
2022-04-21 14:09:06 +0200

    Create Item Model with EF support

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50), Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+    }
+}
